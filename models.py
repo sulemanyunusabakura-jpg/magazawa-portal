29cdfc5d08ae8399d1a1ae1f932de25d3751ccc6
sulemanyunusabakura-jpg <sulemanyunusabakura@gmail.com>
2026-08-21 07:00:21 -0700

    Update models.py

--- models.py
+++ models.py
@@ -1,10 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 from datetime import datetime
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'user'
+    
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
