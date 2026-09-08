666e8bf53dcb4c734690e4adea5ff1734179e86d
sulemanyunusabakura-jpg <sulemanyunusabakura@gmail.com>
2026-09-08 12:33:22 -0700

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -1660,6 +1660,12 @@ def fix_results_db():
     return f'Database Error: {str(e)}'
 
 
+# --- ROUTE TO SERVE UPLOADED FILES PERMANENTLY ---
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
 if __name__ == '__main__':
   port = int(os.environ.get('PORT', 5000))
   app.run(host='0.0.0.0', port=port)
